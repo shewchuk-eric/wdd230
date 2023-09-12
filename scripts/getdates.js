2775f81f827ca09e421e2a1b9ec676d098a7f1e6
Eric Shewchuk <eric@thebanyanteam.com>
2023-09-11 16:58:47 -1000

    made a lot of progress on home page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,6 @@
 
 
 const options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
     year: "numeric"
 };
 const today = document.querySelector('#date').innerHTML = new Date().toLocaleDateString("en-US", options);
