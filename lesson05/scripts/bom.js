3eb876deacdbea0a9408220f3e8f51582953b631
Eric Shewchuk <eric@thebanyanteam.com>
2023-10-07 12:49:37 -1000

    found 1 bug

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -1,8 +1,6 @@
 const input = document.querySelector('input'); // get value of input field
 const submit = document.querySelector('button');
 const chapList = document.querySelector('#list');
-const listItem = document.createElement('li');
-const killButton = document.createElement('button');            //.innerHTML = '<button class="remove">❌</button>';
 
 submit.addEventListener('click', function () { // watches for click on button
     if (input.value == '') { // checks to see that information has been entered
@@ -10,15 +8,19 @@ submit.addEventListener('click', function () { // watches for click on button
         submit.focus();
         exit;
     }
+    const listItem = document.createElement('li');
+    const killButton = document.createElement('button');
     listItem.textContent = input.value;                 //innerHTML = `${input.value}  ${killButton}`;
     killButton.textContent = '❌';                  //chapList.appendChild(listItem);
     listItem.append(killButton);
     chapList.append(listItem);
+
+    killButton.addEventListener('click', function () {
+        chapList.removeChild(li);
+        input.focus();
+        input.value = '';
+    });
 });
 
                                                           //const deleteB = document.querySelector('.remove');
-killButton.addEventListener('click', function () {
-    chapList.removeChild(li);
-    input.focus();
-    input.value = '';
-});
+
