11b9b2a14f52ea2dc4bcb5313246bee25d8cf428
Eric Shewchuk <eric@thebanyanteam.com>
2023-10-12 21:49:35 -1000

    progress on week 5

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -18,3 +18,20 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+
+// code to toggle dark mode
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
