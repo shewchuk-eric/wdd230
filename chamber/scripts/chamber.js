7a306f239d93d3383a4cd0148f8987014f756522
Eric Shewchuk <eric@thebanyanteam.com>
2023-10-14 14:31:44 -1000

    finished week 5

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -22,16 +22,38 @@ hamButton.addEventListener('click', () => {
 
 // code to toggle dark mode
 const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
+const body = document.querySelector("body");
+const html = document.querySelector("html");
+const h2 = document.querySelectorAll("h2");
+// const a = document.querySelectorAll("section a");
+const a = document.querySelectorAll("a");
+const cta = document.querySelector(".cta a");
+const active = document.querySelector(".active");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("Use Dark Mode")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
+		body.style.background = "#353535";
+		body.style.color = "#d9d9d9";
+		html.style.background = "#000";
+		for (let i=0; i<h2.length; i++) {
+			h2[i].style.color = "#68bcc1";
+		}
+		for (let i=0; i<a.length; i++) {
+			a[i].style.color = "#b0ebef";
+		}
 		modeButton.textContent = "Use Light Mode";
 	} else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
+		body.style.background = "white";
+		body.style.color = "#353535";
+		html.style.background = "#353535";
+		for (let i=0; i<h2.length; i++) {
+			h2[i].style.color = "#284b63";
+		}
+		for (let i=0; i<a.length; i++) {
+			a[i].style.color = "#284b63";
+		}
+		cta.style.color = "#d9d9d9";
+		active.style.color = "#d9d9d9";
 		modeButton.textContent = "Use Dark Mode";
 	}
 });
